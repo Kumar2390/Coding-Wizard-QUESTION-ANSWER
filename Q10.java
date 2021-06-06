7714f5f8cd4702cc1d4dd2aacda70203307ca17f
Ashwini kumar behera <79761853+Kumar2390@users.noreply.github.com>
2021-06-06 13:23:52 +0530

    Update Q10.java

--- Q10.java
+++ Q10.java
@@ -1,28 +1,51 @@
 //Write a program that accepts an integer (n) and computes the value of n+nn+nnn.
 
-import java.util.Scanner;
 public class Q10 {
-	
-	public static int solve(int n) {
-		
-	int n1 = n;
-	int n2 = (n * 10) + n;
-	int n3 = (n * 10 * 10)+n2;
-	
-	return n1 + n2 + n3;
-		
-		
+	static int cal(int n) {
+		int sum=0;
+		 for (int i = 1; i <=3; i++)
+	        {
+	            String number = "";
+	            for (int j = 0; j < i; j++)
+	            {
+	                number = number + n;
+	            }
+	          
+	            sum += Integer.parseInt(number);
+	        }
+	       
+	      return sum; 
 	}
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter any number: ");
-		int n = sc.nextInt();
-		
-		System.out.println("Output: "+solve(n));
-		sc.close();
-	
-    	}
+	int n=10;
+	System.out.println("the value of n+nn+nnn for "+n+" is "+ cal(n)); // here it calculate as (10 + 1010 + 101010)
+
+	}
 
 }
+
+
+	
+
+		
+
+	
+	
+	
+	
+		
+		
+	
+
+
+	
+		
+		
+		
+		
+		
+
+	
+    	
+
